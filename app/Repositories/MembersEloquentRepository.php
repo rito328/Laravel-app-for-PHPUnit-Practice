34c03ff16011ca6fce28fa5950878fae7fc61b70
rito <rito@ritonoMacBook-Pro.local>
2019-04-28 18:40:15 +0900

    add Exception

--- app/Repositories/MembersEloquentRepository.php
+++ app/Repositories/MembersEloquentRepository.php
@@ -19,6 +19,7 @@ class MembersEloquentRepository implements MembersRepositoryInterface
 
     public function insert(string $name): void
     {
+        if ($name==='') throw new \Exception('Argument is empty.');
         $member = new Member;
         $member->name = $name;
         $member->save();
@@ -26,6 +27,7 @@ class MembersEloquentRepository implements MembersRepositoryInterface
 
     public function update(int $id, string $name): void
     {
+        if ($name==='') throw new \Exception('Argument $name is empty.');
         $member = Member::find($id);
         $member->name = $name;
         $member->save();
